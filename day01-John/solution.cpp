99b13ea305fcbd0c9a74ede747399caa842cda3f
John Hartshorne <87952311+Jjmc1313@users.noreply.github.com>
2025-12-01 01:12:07 -0800

    Create solution.cpp

---
+++ day01-John/solution.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "dial.hpp"
+
+int main() {
+    dial dial;
+    
+    std::ifstream file("input.txt");
+    std::string line;
+
+    if (file.is_open()) {
+        char rotationDirection;
+        int rotationAmount;
+
+        while (std::getline(file, line)) {
+            rotationDirection = line.at(0);
+
+            rotationAmount = std::stoi(line.substr(1, line.size()));
+
+            dial.rotate(rotationDirection, rotationAmount);
+            dial.rotateThrough(rotationDirection, rotationAmount);
+        }
+
+        file.close();
+    } else {
+        std::cerr << "Unable to open file!" << std::endl;
+    }
+
+    std::cout << "Zeros: " << dial.getZeros() << std::endl
+              << "Zeros Through: " << dial.getZerosThrough() << std::endl;
+
+    return 0;
+}

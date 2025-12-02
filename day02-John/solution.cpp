563d9188e514c4e7ad95c0960291bef8f5827b7c
J_jmc <johnlhartshorne@gmail.com>
2025-12-01 23:29:42 -0800

    Removed redunant function, still bad lol

--- day02-John/solution.cpp
+++ day02-John/solution.cpp
@@ -4,6 +4,14 @@
 #include <vector>
 #include <fstream>
 
+#define DEBUG 0
+
+/* 
+INPUT FILE MUST NOT BE CSV, VALUES MUST BE ON NEW LINE:
+123-456
+789-101112
+*/
+
 struct Range {
     std::string low, high;
 
@@ -11,6 +19,7 @@ struct Range {
         std::string temp;
         std::smatch m;
 
+        // match the first number in a line, and define low as it
         regex_search(input, m, std::regex("\\d*"));
         for (auto x : m) {
             temp.append(x);
@@ -19,6 +28,7 @@ struct Range {
         this->low = temp;
         temp = "";
 
+        // match the second number in a line, and define high as it
         regex_search(input, m, std::regex("\\d*$"));
         for (auto x : m) {
             temp.append(x);
@@ -28,15 +38,15 @@ struct Range {
     }
 };
 
-void partOne(std::vector<Range>);
-void partTwo(std::vector<Range>);
+void solve(std::vector<Range>, int);
 
 int main() {
     std::ifstream file("input.txt");
     std::string line;
-    std::vector<Range> rangeList;
+    std::vector<Range> rangeList; // vector containing range object for each line of input
     int vectorPosition = 0;
 
+    // Append each line of input to rangeList with properly seperated values
     if (file.is_open()) {
         while (std::getline(file, line)) {
             rangeList.push_back(Range(line));
@@ -45,54 +55,44 @@ int main() {
         std::cerr << "Unable to open file!" << std::endl;
     }
 
-    partOne(rangeList);
-    partTwo(rangeList);
+    solve(rangeList, 1);
+    solve(rangeList, 2);
 
     return 0;
 }
 
-void partOne(std::vector<Range> rangeList) {
-    const std::regex pattern("^(.*)\\1$");
+void solve(std::vector<Range> rangeList, int part) {
+    std::regex pattern;
+    if (part == 1) {
+        pattern = "^(.*)\\1$"; // Matches if first and second half of string are the same
+    } else if (part == 2) {
+        pattern = "(\\d*)\\1+"; // Matches if any sequence of numbers repeats in an entire string 
+    }
 
     uint64_t sum = 0;
 
     uint64_t lowTemp, highTemp;
     for (auto x : rangeList) {
+        // Cast x.low and x.high to uint64_t (long long)
         lowTemp = std::stoll(x.low);
         highTemp = std::stoll(x.high);
 
-        // std::cout << lowTemp << " " << highTemp << std::endl;
+        #if DEBUG
+        std::cout << lowTemp << " " << highTemp << std::endl;
+        #endif 
+        // For each number between low and high of a range object, check if it matches the specified pattern
         for (uint64_t i = lowTemp; i <= highTemp; i++) {
             if (std::regex_match(std::to_string(i), pattern)) {
                 sum = sum + i;
-                // std::cout << "HIT [" << lowTemp << "-" << highTemp << "] : " << i << std::endl;
+                #if DEBUG
+                std::cout << "HIT [" << lowTemp << "-" << highTemp << "] : " << i << std::endl;
+                #endif
             }
         }
-        // std::cout << std::endl;
+        #if DEBUG
+        std::cout << std::endl;
+        #endif
     }
 
-    std::cout << "Part One Sum: " << sum << std::endl;
-}
-
-void partTwo(std::vector<Range> rangeList) {
-    std::regex pattern("(\\d*)\\1+");
-
-    uint64_t sum = 0;
-
-    uint64_t lowTemp, highTemp;
-    for (auto x : rangeList) {
-        lowTemp = std::stoll(x.low);
-        highTemp = std::stoll(x.high);
-
-        // std::cout << lowTemp << " " << highTemp << std::endl;
-        for (uint64_t i = lowTemp; i <= highTemp; i++) {
-            if (std::regex_match(std::to_string(i), pattern)) {
-                sum = sum + i;
-                // std::cout << "HIT [" << lowTemp << "-" << highTemp << "] : " << i << std::endl;
-            }
-        }
-        // std::cout << std::endl;
-    }
-
-    std::cout << "Part Two Sum: " << sum << std::endl;
+    std::cout << "Part " << part << " Sum: " << sum << std::endl;
 }
